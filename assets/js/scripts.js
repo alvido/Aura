a7e9b4e8b88ae498c05a81ad6eed8f4cc83211f0
Oleksandr Dorobalo <al.dorobalo@gmail.com>
2024-10-03 00:05:11 +0200

    front page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let burgerButton = document.getElementById("burgerButton");
   let navigation = document.querySelector(".navigation");
   let links = document.querySelectorAll(".navigation__link");
+  let body = document.querySelector("body");
 
   // Если бургер-кнопка существует, добавляем обработчик события
   if (burgerButton) {
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
       e.stopPropagation(); // Остановка всплытия события
       burgerButton.classList.toggle("burger--active"); // Переключаем класс активности бургер-кнопки
       navigation.classList.toggle("navigation--active"); // Переключаем класс активности навигации
+      body.classList.toggle("lock"); // Переключаем класс активности навигации
     });
   }
 
@@ -18,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     link.addEventListener("click", function (e) {
       burgerButton.classList.remove("burger--active");
       navigation.classList.remove("navigation--active");
-      console.log("link", link);
+      body.classList.remove("lock");
     });
   });
 });
@@ -47,5 +49,111 @@ $(document).ready(function () {
       main.css("padding-top", 0); // Убираем верхний отступ
     }
   }
-
 });
+
+// menu has children //
+document.addEventListener("DOMContentLoaded", function () {
+  // Выбираем все элементы с классом "has-children"
+  let hasChildren = document.querySelectorAll(".has-children");
+
+  // Если такие элементы существуют, добавляем обработчик события
+  if (hasChildren) {
+    hasChildren.forEach((children) => {
+      // Обработчик клика по элементу "has-children"
+      children.addEventListener("click", function (e) {
+        e.stopPropagation(); // Останавливаем всплытие события, чтобы оно не доходило до документа
+        this.classList.toggle("active");
+      });
+    });
+
+    // Обработчик клика по документу
+    document.addEventListener("click", function (e) {
+      hasChildren.forEach((children) => {
+        // Проверяем, если клик произошел вне элемента "has-children"
+        if (!children.contains(e.target)) {
+          children.classList.remove("active");
+        }
+      });
+    });
+  }
+});
+
+//
+
+//swiper
+document.addEventListener("DOMContentLoaded", function () {
+  // Функция для инициализации слайдера
+  function initSwiper(selector, paginationClass, nextButtonClass, prevButtonClass) {
+    if (document.querySelector(selector)) {
+      new Swiper(selector, {
+        observer: true,
+        // centeredSlides: true,
+        observeParents: true,
+        loop: true,
+        // autoplay: {
+        //   delay: 3000,
+        //   disableOnInteraction: false,
+        // },
+        pagination: {
+          el: paginationClass,
+          clickable: true,
+        },
+        navigation: {
+          nextEl: nextButtonClass,
+          prevEl: prevButtonClass,
+        },
+        // // And if we need scrollbar
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        // },
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 6,
+          },
+        },
+      });
+    }
+  }
+
+  // Инициализация слайдеров
+  if (document.querySelector("#hero")) {
+    new Swiper("#hero", {
+      observer: true,
+      observeParents: true,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".hero-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".hero-button-next",
+        prevEl: ".hero-button-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 8,
+        },
+      },
+    });
+  }
+
+  // Инициализация слайдеров для других секций
+  initSwiper("#categories", "", ".categories-button-prev", ".categories-button-next");
+});
+
+// swiper
